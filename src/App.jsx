2daabd69915f423521f3ffd509eb4f6dd803812f
Govind Dandekar <govind@Govinds-Mac-mini.local>
2024-08-11 10:54:23 -0700

    Add Player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+// capitilization on "PLayer" doesn't match Player.jsx
+import Player from './components/PLayer.jsx';
 
 function App() {
   
-
-  return (
-    <h1>React Tic-Tac-Toe</h1>
+return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
   )
 }
 
