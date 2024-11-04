13cd119ed30c51de6f04d329d87810fd23ffb416
Govind Dandekar <govind@Govinds-Mini.lan>
2024-11-03 18:11:25 -0800

    Add playerName state

---
+++ src/components/Player.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function Player({initialName, symbol}){
+	
+	const [ playerName, setPlayerName ] = useState(initialName);
+	const [ isEditing, setIsEditing ] = useState(false);
+
+	function handleEditClick(){
+		setIsEditing(editing => !editing)
+	}
+
+	function handleChange(event){
+		setPlayerName(event.target.value)
+	}
+
+	return (
+		<li>
+            <span className="player">
+              {isEditing ? 
+			  	<input
+					onChange={handleChange}
+					value={playerName}
+					type="text" 
+					required 
+				/> :
+				<span className="player-name">{playerName}</span>
+			  }
+              <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditClick}>
+				{ isEditing ? "Save" : "Edit" }
+			</button>
+        </li>
+	)
+}
+
+export default Player;
